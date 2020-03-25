a3a148c22dc943dd1b71f3d308d54b5d02a9dda1
Aakansha Doshi <aakansha1216@gmail.com>
2020-03-25 23:36:10 +0530

    feat(index.js): add checks to filter bad commit messages

---
+++ constants.js
@@ -0,0 +1,3 @@
+const BAD_KEYWORDS = ['temp', 'tmp', 'fixup', 'squash', 'wip'];
+const MIN_COMMIT_MESSAGE_LENGTH = 10;
+module.exports = { BAD_KEYWORDS, MIN_COMMIT_MESSAGE_LENGTH };
---
+++ index.js
@@ -0,0 +1,55 @@
+const { getInput, setFailed, debug } = require('@actions/core');
+const { GitHub, context } = require('@actions/github');
+
+const { BAD_KEYWORDS, MIN_COMMIT_MESSAGE_LENGTH } = require('./constants');
+
+function filterCommit(commit) {
+  commit = commit.toLowerCase();
+  const result = [];
+  for(keyword of BAD_KEYWORDS) {
+    if (commit.indexOf(keyword) != -1) {
+      result.push(keyword);
+    }
+  }
+
+  return result;
+}
+
+async function verifyCommits(repoToken) {
+  const client = new GitHub(repoToken);
+  const { data: commits } = client.pulls.listCommits({ owner: context.repo.repo, repo: context.repo.repo, pull_number: context.issue.number})
+  debug(`There are ${commits.length} commits`);
+
+  let errors = [];
+  for (const commit of commits) {
+    const { message } = commit.commit;
+    debug(message);
+
+    if (message.length < MIN_COMMIT_MESSAGE_LENGTH) {
+      errors.push(`${message} has less than ${MIN_COMMIT_MESSAGE_LENGTH} characters`);
+    }
+
+    if (message[0] !== message[0].toUpperCase()) {
+      errors.push(`${message} should have first letter in upper case`);
+    }
+
+    const badKeywords = filterCommit(message);
+    if (badKeywords.length) {
+      errors.push(`${message} contains ${badKeywords.join()}`);
+    }
+  }
+  if (errors.length) {
+    throw Error(errors);
+  }
+}
+async function main() {
+  try {
+    const repoToken = getInput('repo-token', { required: true });
+    await verifyCommits(repoToken);
+    debug('Recieved repo token');
+  } catch (e) {
+    setFailed(`Action failed with error ${e.message}`);
+  }
+}
+
+main();
